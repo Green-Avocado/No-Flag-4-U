a766724510f26eb5225ab67d1693cff6a6c7a8a2
Green-Avocado <greenavocado@protonmail.com>
2022-05-30 00:01:08 -0700

    init cargo project

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+#[no_mangle]
+pub extern "C" fn free() -> () {
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
