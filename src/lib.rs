6b065884d499f8d5081586875941384a4fe955d7
Green-Avocado <greenavocado@protonmail.com>
2022-05-30 01:46:56 -0700

    removed stubs

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,6 @@
 #[no_mangle]
 pub extern "C" fn free() {}
 
-#[no_mangle]
-pub extern "C" fn malloc() {}
-
-#[no_mangle]
-pub extern "C" fn read() {}
-
-fn pointer_is_stack() -> bool {
-    true
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
