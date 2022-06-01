8bd363dbd023f8c780c9d0500598d2ddf2d6ff0d
Green-Avocado <greenavocado@protonmail.com>
2022-05-31 21:55:29 -0700

    replace assembly call with raw function pointer

--- src/preload_hooks/memory_management.rs
+++ src/preload_hooks/memory_management.rs
@@ -1,6 +1,6 @@
 use crate::{utils::get_ptr_info, LIBC_PATH, MAIN_STARTED};
 use libc::{c_void, dlclose, dlopen, dlsym, RTLD_LAZY, RTLD_LOCAL};
-use std::{arch::asm, cell::Cell, ffi::CString, panic, sync::atomic::Ordering};
+use std::{cell::Cell, ffi::CString, mem, panic, sync::atomic::Ordering};
 
 thread_local! {
     static FREE_RECURSION_GUARD: Cell<bool> = Cell::new(true);
@@ -19,20 +19,18 @@ pub extern "C" fn free(ptr: *mut c_void) {
     FREE_RECURSION_GUARD.set(false);
 
     if !MAIN_STARTED.load(Ordering::SeqCst) {
+        let real_sym: extern "C" fn(*mut c_void);
+
         unsafe {
             let handle = dlopen(
                 CString::new(LIBC_PATH).unwrap().into_raw(),
                 RTLD_LAZY | RTLD_LOCAL,
             );
-            let real_sym = dlsym(handle, CString::new("free").unwrap().into_raw());
+            real_sym = mem::transmute(dlsym(handle, CString::new("free").unwrap().into_raw()));
             dlclose(handle);
-
-            asm!(
-                "call rax",
-                in("rax") real_sym,
-                in("rdi") ptr,
-            );
         }
+
+        real_sym(ptr);
     } else {
         let page_info = get_ptr_info(ptr).expect("freeing invalid pointer");
 
