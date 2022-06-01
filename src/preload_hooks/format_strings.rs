b7b218e2e78c98bdc0531ff0bf7737aa140b8c4a
Green-Avocado <greenavocado@protonmail.com>
2022-06-01 00:37:33 -0700

    simplify prinf with variadic args

--- src/preload_hooks/format_strings.rs
+++ src/preload_hooks/format_strings.rs
@@ -1,18 +1,16 @@
 use crate::utils::get_ptr_info;
 use libc::{c_char, c_void, dlsym, RTLD_NEXT};
 use std::{
-    arch::asm,
     ffi::{CStr, CString},
-    panic,
+    mem, panic,
 };
 
+// TODO: hook vprintf and use printf as a wrapper for vprintf
+
+// TODO: increase limit of args (currently <= 6)
 #[no_mangle]
 pub unsafe extern "C" fn printf(mut format: *const c_char, mut args: ...) {
     let mut arg2: usize = args.arg();
-    let arg3: usize = args.arg();
-    let arg4: usize = args.arg();
-    let arg5: usize = args.arg();
-    let arg6: usize = args.arg();
 
     let page_info = get_ptr_info(format as *const c_void).expect("invalid format string pointer");
 
@@ -37,18 +35,17 @@ pub unsafe extern "C" fn printf(mut format: *const c_char, mut args: ...) {
         }
     }
 
-    let real_sym = dlsym(RTLD_NEXT, CString::new("printf").unwrap().into_raw());
+    let real_sym: extern "C" fn(*const c_char, ...) =
+        mem::transmute(dlsym(RTLD_NEXT, CString::new("printf").unwrap().into_raw()));
 
-    asm!(
-        "leave",
-        "jmp rax",
-        in("rax") real_sym,
-        in("rdi") format,
-        in("rsi") arg2,
-        in("rdx") arg3,
-        in("rcx") arg4,
-        in("r8") arg5,
-        in("r9") arg6,
-        options(noreturn),
+    real_sym(
+        format,
+        arg2,
+        args.arg::<usize>(),
+        args.arg::<usize>(),
+        args.arg::<usize>(),
+        args.arg::<usize>(),
+        args.arg::<usize>(),
+        args.arg::<usize>(),
     );
 }
