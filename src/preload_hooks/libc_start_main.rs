1fee7d81316c2593333bc02d54ac12db812839b9
Green-Avocado <greenavocado@protonmail.com>
2022-06-01 00:33:11 -0700

    simplify using variadic

--- src/preload_hooks/libc_start_main.rs
+++ src/preload_hooks/libc_start_main.rs
@@ -5,13 +5,7 @@ use std::{ffi::CString, mem, panic, process::exit, sync::atomic::Ordering};
 #[no_mangle]
 pub unsafe extern "C" fn __libc_start_main(
     main: extern "C" fn(c_int, *const *const c_char, *const *const c_char) -> c_int,
-    arg2: usize,
-    arg3: usize,
-    arg4: usize,
-    arg5: usize,
-    arg6: usize,
-    arg7: usize,
-    arg8: usize,
+    mut args: ...
 ) {
     if !cfg!(debug_assertions) {
         panic::set_hook(Box::new(|_| {
@@ -22,17 +16,20 @@ pub unsafe extern "C" fn __libc_start_main(
     MAIN_STARTED.store(true, Ordering::SeqCst);
 
     let real_sym: extern "C" fn(
-        main: extern "C" fn(c_int, *const *const c_char, *const *const c_char) -> c_int,
-        arg2: usize,
-        arg3: usize,
-        arg4: usize,
-        arg5: usize,
-        arg6: usize,
-        arg7: usize,
-        arg8: usize,
+        extern "C" fn(c_int, *const *const c_char, *const *const c_char) -> c_int,
+        ...
     ) = mem::transmute(dlsym(
         RTLD_NEXT,
         CString::new("__libc_start_main").unwrap().into_raw(),
     ));
-    real_sym(main, arg2, arg3, arg4, arg5, arg6, arg7, arg8);
+    real_sym(
+        main,
+        args.arg::<usize>(),
+        args.arg::<usize>(),
+        args.arg::<usize>(),
+        args.arg::<usize>(),
+        args.arg::<usize>(),
+        args.arg::<usize>(),
+        args.arg::<usize>(),
+    );
 }
