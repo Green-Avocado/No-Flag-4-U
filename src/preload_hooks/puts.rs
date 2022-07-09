683727d60230a37bd040fcc3849c8d45416ef42e
Green-Avocado <greenavocado@protonmail.com>
2022-07-09 03:00:44 -0700

    change order

--- src/preload_hooks/puts.rs
+++ src/preload_hooks/puts.rs
@@ -6,6 +6,30 @@ extern "C" {
     static stdout: *mut FILE;
 }
 
+/// Hooks `puts`.
+///
+/// Passes call to `puts` in libc.
+///
+/// # Safety
+///
+/// Ensure that `s` is a valid C string.
+#[no_mangle]
+pub unsafe extern "C" fn puts(s: *const c_char) -> c_int {
+    utils::log(
+        format!(
+            "puts(s=&\"{ptr_contents}\")\n",
+            ptr_contents = CStr::from_ptr(s)
+                .to_str()
+                .expect("invalid string passed to puts"),
+        )
+        .as_str(),
+    );
+
+    let real_puts: extern "C" fn(*const c_char) -> c_int =
+        mem::transmute(utils::dlsym_next("puts"));
+    real_puts(s)
+}
+
 /// Hooks `fputs`.
 ///
 /// Passes call to `fputs` in libc.
@@ -33,28 +57,4 @@ pub unsafe extern "C" fn fputs(s: *const c_char, stream: *mut FILE) -> c_int {
     let real_fputs: extern "C" fn(*const c_char, *mut FILE) -> c_int =
         mem::transmute(utils::dlsym_next("fputs"));
     real_fputs(s, stream)
-}
-
-/// Hooks `puts`.
-///
-/// Passes call to `puts` in libc.
-///
-/// # Safety
-///
-/// Ensure that `s` is a valid C string.
-#[no_mangle]
-pub unsafe extern "C" fn puts(s: *const c_char) -> c_int {
-    utils::log(
-        format!(
-            "puts(s=&\"{ptr_contents}\")\n",
-            ptr_contents = CStr::from_ptr(s)
-                .to_str()
-                .expect("invalid string passed to puts"),
-        )
-        .as_str(),
-    );
-
-    let real_puts: extern "C" fn(*const c_char) -> c_int =
-        mem::transmute(utils::dlsym_next("puts"));
-    real_puts(s)
 }
