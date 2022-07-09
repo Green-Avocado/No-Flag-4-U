5638d8954bd41228ed1428f59e3c8f5cfc01d78c
Green-Avocado <greenavocado@protonmail.com>
2022-07-09 04:02:42 -0700

    update doc build script

--- build-docs.sh
+++ build-docs.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/sh
 
-rm -r docs
+rm -r docs target/doc
+cargo doc --release --no-deps
 echo "<meta http-equiv=\"refresh\" content=\"0; url=no_flag_4_u\">" > target/doc/index.html
 cp -r target/doc docs
